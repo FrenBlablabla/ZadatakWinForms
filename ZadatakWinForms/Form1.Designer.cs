8ac6c8d860c3c7dbd7fcd978c0194dd26ab5224b
franc <franc@FCB>
2026-06-06 11:37:06 +0200

    Ajoutez des fichiers projet.

---
+++ ZadatakWinForms/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ZadatakWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonGet = new Button();
+            buttonPost = new Button();
+            cmbJmbag = new ComboBox();
+            textRésultat = new TextBox();
+            SuspendLayout();
+            // 
+            // buttonGet
+            // 
+            buttonGet.Location = new Point(210, 94);
+            buttonGet.Name = "buttonGet";
+            buttonGet.Size = new Size(75, 23);
+            buttonGet.TabIndex = 0;
+            buttonGet.Text = "GET";
+            buttonGet.UseVisualStyleBackColor = true;
+            // 
+            // buttonPost
+            // 
+            buttonPost.Location = new Point(473, 94);
+            buttonPost.Name = "buttonPost";
+            buttonPost.Size = new Size(75, 23);
+            buttonPost.TabIndex = 1;
+            buttonPost.Text = "POST";
+            buttonPost.UseVisualStyleBackColor = true;
+            // 
+            // cmbJmbag
+            // 
+            cmbJmbag.FormattingEnabled = true;
+            cmbJmbag.Location = new Point(325, 147);
+            cmbJmbag.Name = "cmbJmbag";
+            cmbJmbag.Size = new Size(121, 23);
+            cmbJmbag.TabIndex = 2;
+            // 
+            // textRésultat
+            // 
+            textRésultat.Location = new Point(12, 203);
+            textRésultat.Multiline = true;
+            textRésultat.Name = "textRésultat";
+            textRésultat.Size = new Size(776, 140);
+            textRésultat.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textRésultat);
+            Controls.Add(cmbJmbag);
+            Controls.Add(buttonPost);
+            Controls.Add(buttonGet);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonGet;
+        private Button buttonPost;
+        private ComboBox cmbJmbag;
+        private TextBox textRésultat;
+    }
+}
---
+++ ZadatakWinForms/Form1.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZadatakWinForms
+{
+    public partial class Form1 : Form
+    {
+        private readonly StudentService _service;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _service = new StudentService();
+
+            Load += Form1_Load;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            cmbJmbag.Items.Clear();
+
+            cmbJmbag.Items.Add("Choose");
+
+            var jmbags = await _service.GetJmbags();
+
+            foreach (var item in jmbags)
+            {
+                cmbJmbag.Items.Add(item);
+            }
+
+            cmbJmbag.SelectedIndex = 0;
+        }
+        private async void buttonGet_Click(object sender, EventArgs e)
+        {
+            var jmbags = await _service.GetJmbags();
+
+            textRésultat.Text = string.Join(Environment.NewLine, jmbags);
+        }
+        private async void buttonPost_Click(object sender, EventArgs e)
+        {
+            if (cmbJmbag.SelectedIndex <= 0)
+            {
+                MessageBox.Show("Choose a JMBAG.");
+                return;
+            }
+
+            string jmbag = cmbJmbag.SelectedItem.ToString();
+
+            string result = await _service.SendPost(jmbag);
+
+            textRésultat.Text = result;
+        }
+        public class StudentService
+        {
+            private readonly HttpClient _client;
+
+            public StudentService()
+            {
+                _client = new HttpClient();
+                _client.BaseAddress = new Uri("https://localhost:5032/");
+            }
+
+            public async Task<List<string>> GetJmbags()
+            {
+                return await _client.GetFromJsonAsync<List<string>>
+                    ("api/students/jmbags");
+            }
+
+            public async Task<string> SendPost(string jmbag)
+            {
+                var response = await _client.PostAsJsonAsync(
+                    "api/students",
+                    new { Jmbag = jmbag });
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
